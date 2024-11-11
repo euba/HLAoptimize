d6c85b052cc4fa9447e46dac3de7dc99c68a8528
Eugen Bauer <euba@Eugens-Air.fritz.box>
2024-11-11 15:04:29 +0100

    Added 2D Scatterplot to plot different characteristics of vaccine elements

--- HLAopti/HLAopti.py
+++ HLAopti/HLAopti.py
@@ -40,9 +40,29 @@ def scoreBars(veldata, outplot, title):
 	cols = ['grey' if (x < max(veldata_cnts)) else 'red' for x in veldata_cnts] # color the bars based on the top picks
 
 	### setting up the barplot
-	snsplot = sns.barplot(x="HLA coverage", y="Element", palette=cols, data=veldata, ax=ax) # main plot function
+	snsplot = sns.barplot(x="HLA coverage", y="Element", 
+			palette=cols, data=veldata, ax=ax) # main plot function
 	snsplot.set_title(title, size=50) # setting the plot title based on selected virus
 	snsplot.set_xlabel('HLA coverage', size=20) # setting size of x-axis label
 	snsplot.set_ylabel('Element', size=20) # setting size of y-axis label
+	
 	snsplot.figure.savefig(outplot) # saving the plot
+
+def score2D(veldata, outplot, title):
+	veldata_cnts = veldata["HLA coverage"] # taking out the HLA coverage for downstream calculation
+
+	### canvas parameters
+	fig, ax = plt.subplots(figsize=(10, 10)) # scaling the height/width of plot
+	veldata["Pick"] = ['other' if (x < max(veldata_cnts)) else 'top' for x in veldata_cnts] # color the points based on the top picks
+	
+	### setting up the scatterplot
+	scat2D = sns.scatterplot(x="HLA coverage", y="Immunogenicity", size="Error",
+			hue="Pick", palette=['red','grey'], 
+			sizes=(20, 200), data=veldata, ax=ax) # main plotting function
+	scat2D.set_title(title, size=18) # setting the plot title based on selected virus
+	scat2D.set_xlabel('HLA coverage', size=15) # setting size of x-axis label
+	scat2D.set_ylabel('Immunogenicity (Median)', size=15) # setting size of y-axis label
+	sns.move_legend(scat2D, "upper left", bbox_to_anchor=(1, 1)) # shifting the legend to the left
+	
+	scat2D.figure.savefig(outplot) # saving the plot
 
